f5376cf9588c56ec6acbd977e15b1afa6ca3fab0
BelAec <morgannaf11@outlook.com>
2023-06-17 23:30:11 -0400

    displayContatcs

--- search.js
+++ search.js
@@ -2,65 +2,28 @@ const userCardTemplate = document.querySelector("[data-user-template]")
 const userCardContainer = document.querySelector("[data-user-cards-container]")
 const searchInput=document.querySelector("[data-search]")
 const selectCard = document.querySelector("[data-card]")
-const copyCard = document.querySelector("[data-copy]")
 const button = document.querySelector("[button]")
-
 
 //const btn = document.getElementById('btn-select');
 let contactInfo="";
 let users=[];
 
-
-
 searchInput.addEventListener("input", e => {
     const value = e.target.value.toLowerCase()
-    users.forEach(user => {
-      const isVisible =
-      user.name.toLowerCase().includes(value); 
-      
-      user.element.classList.toggle("hide", !isVisible)
-             
-      
-      if(value === ""){
-        hideContacts();
-      }else{
-        showContacts();
-      
-    
-       
-    //   ||user.Email.toLowerCase().includes(value);
-       
-      }   
+      users.forEach(user => {
+        const isVisible = 
+        user.name.toLowerCase().includes(value); 
+        
+        user.element.classList.toggle("hide", !isVisible);
+        
+          if(value === ""){
+            hideContacts();
+          }else{
+            showContacts();
+          }
+       });
     })
-
-  })
-
-  // searchInput.addEventListener("input", e=>{
-  
-  // })
-  // function resetSearch(){
-  //     let form=document.getElementById('searchForm');
-  //   form.reset();
-  // }
-  
-  // var copy = document.querySelectorAll(".copy");
-          // let toCopy = $(".copy:visible"); 
-       
-          //  toCopy.onclick = function() { 
-          //    document.execCommand("copy"); 
-           
-          //    toCopy.addEventListener("copy", function(event) { 
-          //    event.preventDefault(); 
-             
-          //    if (event.clipboardData) { 
-          //     event.clipboardData.setData("text/plain", toCopy.textContent);
-          //     //console.log(event.clipboardData.getData("text"))
-          //     let result = event.clipboardData.getData("text");
-          //     document.getElementById('test').innerHTML = "<br>\n"+result.textContent;
-          //     }
-          //   })
-          // }
-   
+ 
 //pull in json data via url 
 //   fetch("https://jsonplaceholder.typicode.com/users")
   
@@ -72,21 +35,24 @@ fetch('./contactsFinal.json') //this loads black boxes
     //these are inside the card
     
         const card = userCardTemplate.content.cloneNode(true).children[0];  //clone this content and all content indisd, returns a doc fracgemant
-        const header = card.querySelector("[data-header]")
+        const header = card.querySelector("[data-header]")//name
         const address = card.querySelector("[data-address]")
         const ville = card.querySelector("[data-ville]")
         const region = card.querySelector("[data-region]")
+        const codePostal = card.querySelector("[data-codePostal]")
         const tel = card.querySelector("[data-tel]")
-        const body = card.querySelector("[data-body]")
-       // const checkbox = card.querySelector("[data-checkbox]");
+        const body = card.querySelector("[data-body]")//email box
+       
         //const button = button.querySelector("[data-button]");
+       
         //now can do this
-        header.textContent = user.Name;
-        address.textContent=user.Addresse;
-        ville.textContent= user.Ville;
-        region.textContent= user.Region;
-        tel.textContent=user.Phone;
-        body.textContent = user.Email;
+        header.textContent = user.Name+'<br>';
+        address.textContent= user.Addresse+'<br>'
+        ville.textContent = user.Ville+', '
+        region.textContent = user.Region+', '
+        codePostal.textContent=user.CodePostal+'<br>'
+        tel.textContent = user.Tel+'<br>'
+        body.textContent = user.Email
        
         
         //so we want a header and the body from our card->go back to html file and add attributes
@@ -95,27 +61,17 @@ fetch('./contactsFinal.json') //this loads black boxes
         
 
         userCardContainer.append(card); 
-       // testCheck(card);
-        //function displayCard(card){
-         
-           //let selectedCard = document.querySelector('.card');
-         
-       //function displayCard(card){
-         
-          //  let mycards = document.getElementsByClassName('card');
-          //  let attr = "card";
-          //  for(i=0; i <mycards.length; i++){
-          //  mycards[i].setAttribute('class', 'card');
-                            
-          //  }      
+          
           
 
-        return contactInfo = {name :user.Name, 
-        email: user.Email, 
+        return contactInfo = 
+        {name :user.Name, 
         Addresse: user.Addresse,
         Ville: user.Ville,
         Region: user.Region,
+        CodePostal: user.codePostal,
         Tel: user.Phone, 
+        Email: user.Email, 
         element : card}
     
     })
@@ -123,56 +79,13 @@ fetch('./contactsFinal.json') //this loads black boxes
    
 }) //end of user.map
 let isSelected=false;
-
-// function addSelect(){
-//   let mycards =  document.querySelectorAll('.card');
-//   mycards.forEach(el=>{el.append('')
-
-//   })
-// };
-
- // function testCheck(){  
-// function testCheck(card){
-
-//   let selectBox = document.querySelector("input[name=checkbox]");
-//   selectBox.addEventListener('change', function(){
-//     if(this.checked===false){ 
-//       return -1;
-//   }
-//       else if(this.checked === true){
-//         document.getElementById('test').innerHTML = "test" ; //+ card.textContent;
-//       }
-//     // 
-//     })
-//   }
   
-
-// function testCheck(){
-  
-//   let selectBox = document.querySelector(".checkbox");
-//     if(selectBox.checked===false){
-//      return -1;
-//     }
-//     else if(selectBox.checked === true){
-//       console.log(selectBox)
-    
- 
-//     // document.getElementById('test').innerHTML = "hello";
-//    }
-//   }
-  //contactsSelected(isSelected);
-    
-  
-
-
-//contactsSelected(isSelected);
-                   
-// function contactsSelected(isSelected) {
-
  function getContacts (button){
+
     //document.getElementById('btn-card').innerHTML = "";
     //document.getElementById('user-cards').textContent;
-    let coordinates = button.parentNode.textContent+'\n';
+    let coordinates = button.parentNode.textContent;
+    // document.getElementById('test').innerHTML = coordinates.Email;
     document.getElementById('test').innerHTML = coordinates;
     
  }
@@ -219,4 +132,16 @@ let isSelected=false;
  }
 
         
-    
+   // testCheck(card);
+        //function displayCard(card){
+         
+           //let selectedCard = document.querySelector('.card');
+         
+       //function displayCard(card){
+         
+          //  let mycards = document.getElementsByClassName('card');
+          //  let attr = "card";
+          //  for(i=0; i <mycards.length; i++){
+          //  mycards[i].setAttribute('class', 'card');
+                            
+          //  }    
